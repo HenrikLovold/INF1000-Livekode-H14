2db551dbaf4de9dd67917909e0015cbf24815061
Henrik Hillestad Lovold <henrihlo@ifi.uio.no>
2014-09-04 14:10:58 +0200

    Kode far termtime 04/09
    Her er koden fra terminaltimen torsdag 04/09. Koden viser en klasse med
    main, og én funksjon som tar i mot to argumenter (int) og skriver dem
    ut.

---
+++ Uke02/MedArgumenter.java
@@ -0,0 +1,17 @@
+class MedArgumenter{
+    
+    public static void main(String[] args){
+
+	int a = 4;
+	int b = 2345;
+
+	toTall(a, b);
+    }
+
+    public static void toTall(int x, int y){
+	
+	System.out.println("Tall x: " + x + " Tall y: " + y);
+
+    }
+}
+	
