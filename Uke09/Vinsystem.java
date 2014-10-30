69f4e8edafef6b8b72d8e831e11a9cd7edb4ffa2
Henrik Hillestad L|vold <henrihlo@vetur.ifi.uio.no>
2014-10-30 10:43:35 +0100

    ny vinkode

--- Uke09/Vinsystem.java
+++ Uke09/Vinsystem.java
@@ -1,5 +1,5 @@
 import java.util.*;
-import java.io.File;
+import java.io.*;
 
 public class Vinsystem {
 	
@@ -7,9 +7,56 @@ public class Vinsystem {
 
 	public static void main(String[] args) throws Exception{
 		lesVin();
+		ordrelokke();
+		skrivTilFil();
+	}
+	
+	public static void ordrelokke(){
+		Scanner sc = new Scanner(System.in);
 		
+		loop: while(true){
+			System.out.println("MENY! Dine valg:");
+			System.out.println("A: Avslutt");
+			System.out.println("P: Print alle viner");
+			System.out.println("E: Endre navn pa en vin");
+			
+			String valg = sc.nextLine();
+			
+			switch(valg){
+				case "A":
+				break loop;
+				
+				case "P":
+				printViner();
+				break;
+				
+				case "E":
+				endreVin();
+				break;
+			}	
+		}
+	}
+	
+	public static void printViner(){	
 		for(Vin v : vinliste.values()){	
 			System.out.println(v);
+		}
+	}
+	
+	public static void endreVin(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Hva er navnet pa vinen du vil endre?");
+		String valg = sc.nextLine();
+		
+		if(vinliste.containsKey(valg)){
+			System.out.println("Vinen finnes! Hva er nytt navn?");
+			String nyttNavn = sc.nextLine();
+			vinliste.get(valg).setNavn(nyttNavn);
+			
+			System.out.println("Nytt navn: " + vinliste.get(valg).getNavn());
+			
+		} else {
+			System.out.println("Navnet du tastet finnes ikke");
 		}
 	}
 	
@@ -37,7 +84,24 @@ public class Vinsystem {
 			vinliste.put(navn, v);
 		}
 	}
+	
+	public static void skrivTilFil() throws Exception {
+		File fil = new File("vinliste.txt");
+		PrintWriter pw = new PrintWriter(fil);
+		
+		for(Vin v : vinliste.values()){
+			pw.println(v);
+		}
+		
+		pw.close();
+	}
 }
+
+
+
+
+
+
 
 
 
